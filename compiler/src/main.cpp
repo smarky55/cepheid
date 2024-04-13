fe9ed3518807a9663d8d51688e9b4f224f3baaf6
Sam Kingdon <smarky55@gmail.com>
2024-04-14 00:35:02 +0100

    Load an compile in main

---
+++ compiler/src/main.cpp
@@ -0,0 +1,38 @@
+
+#include <Cepheid.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+static std::string_view usage() {
+  return "Usage: cepheid <input.cep> <output>";
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    std::cout << usage() << std::endl;
+    return EXIT_SUCCESS;
+  }
+
+  std::vector<std::string> args{argv + 1, argv + argc};
+
+  if (args.size() != 2) {
+    std::cerr << "Invalid number of arguments.\n" << usage() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::string src;
+  {
+    std::ifstream infile(args[0]);
+    std::stringstream ss;
+    ss << infile.rdbuf();
+    src = ss.str();
+  }
+
+  Cepheid cep;
+
+  cep.compile(src);
+}
